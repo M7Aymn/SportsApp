b4e2794faf00f5041d3f93036a0612cef0fb18ca
AhmedAshraf14 <a.ashraf.dev@gmail.com>
2024-08-19 18:57:09 +0300

    enhancements to cells and changin assets

--- SportsApp/Modules/All Leagues/View/CustomLeagueCell/LeagueCell.swift
+++ SportsApp/Modules/All Leagues/View/CustomLeagueCell/LeagueCell.swift
@@ -12,6 +12,7 @@ class LeagueCell: UITableViewCell {
     @IBOutlet weak var leagueImage: UIImageView!
     @IBOutlet weak var leagueName: UILabel!
     
+    @IBOutlet weak var view: UIView!
     @IBOutlet weak var button: UIButton!
     var buttonTapped: (() -> Void)?
     
@@ -26,10 +27,14 @@ class LeagueCell: UITableViewCell {
     
     private func setupCellUI() {
         leagueImage.layer.cornerRadius = 16
-        contentView.backgroundColor = UIColor.secondarySystemFill
-        contentView.layer.borderColor = UIColor.systemBrown.cgColor
-        contentView.layer.borderWidth = 1.0
-        contentView.layer.masksToBounds = true
+//        contentView.backgroundColor = UIColor.secondarySystemFill
+//        contentView.layer.borderColor = UIColor.systemBrown.cgColor
+//        contentView.layer.borderWidth = 1.0
+//        contentView.layer.masksToBounds = true
+        
+        view.layer.cornerRadius = 16
+        view.layer.borderColor = UIColor.systemBrown.cgColor
+        view.layer.borderWidth = 1.0
     }
     
     func setupCell(league: LeagueModel) {
